a391e8f5ba80f842524470f8b9c2106df7e5b6ae
jakemstar <74196116+jakemstar@users.noreply.github.com>
2022-12-04 20:56:33 -0600

    cleaned up

--- src/components/box.tsx
+++ src/components/box.tsx
@@ -1,24 +1,12 @@
-import classNames from 'classnames';
-
 type BoxProps = {
     boxProps : {
         letter?: string
-        correct?: string
         row?: number
         column?: number
     }
 }
 
 export default function Box({boxProps}: BoxProps){
-
-    const boxClass = classNames(
-        {"bg-slate-50": boxProps.correct === "X"},
-        {"bg-green-200": boxProps.correct === "Y"},
-        {"bg-orange-200": boxProps.correct === "M"},
-        {"bg-rose-200": boxProps.correct === "N"},
-        
-    );
-
     return (
         <div className={`box-${boxProps.row} box-${boxProps.row}-${boxProps.column} bg-slate-50 text-slate-900 text-3xl uppercase items-center justify-center font-bold inline-flex min-w-[80px] min-h-[80px] rounded box-border shadow-inner`}>
             <div>{boxProps.letter}</div>
--- src/components/row.tsx
+++ src/components/row.tsx
@@ -4,7 +4,6 @@ import Box from "./box"
 type RowProps = {
     guessState : {
         guess: string,
-        correctArray: string[],
         locked: boolean,
         row: number
     }
@@ -15,11 +14,11 @@ export default function Row({guessState}: RowProps) {
 
     return (
         <div className="grid grid-cols-1 gap-4 grid-cols-5 gap-4">
-            <Box boxProps={{letter: guessArray[0], correct: guessState.correctArray[0], row: guessState.row, column: 0}}/>
-            <Box boxProps={{letter: guessArray[1], correct: guessState.correctArray[1], row: guessState.row, column: 1}}/>
-            <Box boxProps={{letter: guessArray[2], correct: guessState.correctArray[2], row: guessState.row, column: 2}}/>
-            <Box boxProps={{letter: guessArray[3], correct: guessState.correctArray[3], row: guessState.row, column: 3}}/>
-            <Box boxProps={{letter: guessArray[4], correct: guessState.correctArray[4], row: guessState.row, column: 4}}/>
+            <Box boxProps={{letter: guessArray[0], row: guessState.row, column: 0}}/>
+            <Box boxProps={{letter: guessArray[1], row: guessState.row, column: 1}}/>
+            <Box boxProps={{letter: guessArray[2], row: guessState.row, column: 2}}/>
+            <Box boxProps={{letter: guessArray[3], row: guessState.row, column: 3}}/>
+            <Box boxProps={{letter: guessArray[4], row: guessState.row, column: 4}}/>
         </div>
     )
 }
